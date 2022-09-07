28352fe41989c9737ff4a03779be56f901b1ba15
SvetlanaZt <svetlana.zt23@gmail.com>
2022-09-07 10:46:33 +0300

    dgd

--- src/components/CurrencyInput/CurrencyInput.jsx
+++ src/components/CurrencyInput/CurrencyInput.jsx
@@ -4,8 +4,8 @@ import css from './CurrencyInput.module.css'
 export default function CurrencyInput(props) {
     return (
       <>
-        <input type="text" value={props.amount} onChange={ev => props.onAmountChange(ev.target.value)} />
-        <select value={props.currency} onChange={ev => props.onCurrencyChange(ev.target.value)}>
+        <input className={css.input} type="text" value={props.amount} onChange={ev => props.onAmountChange(ev.target.value)} />
+        <select className={css.select} value={props.currency} onChange={ev => props.onCurrencyChange(ev.target.value)}>
           {props.currencies.map((currency => (
             <option value={currency}>{currency}</option>
           )))}
